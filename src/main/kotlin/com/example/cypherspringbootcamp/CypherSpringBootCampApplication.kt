ad511f74176c1333fb0ea50cc1c0b48cc07fa4cb
MikataMitsuki <mitsuki.mikata@users.noreply.github.com>
2021-10-18 01:18:54 +0900

    プロジェクト作成

---
+++ src/main/kotlin/com/example/cypherspringbootcamp/CypherSpringBootCampApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cypherspringbootcamp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CypherSpringBootCampApplication
+
+fun main(args: Array<String>) {
+    runApplication<CypherSpringBootCampApplication>(*args)
+}
