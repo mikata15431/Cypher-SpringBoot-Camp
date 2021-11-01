8f437d71b94f1bf7a4bd87082bf36f35628ec022
MikataMitsuki <mitsuki.mikata@users.noreply.github.com>
2021-11-01 23:03:43 +0900

    Jsonを直接返すのではなく、data classを返す

--- src/main/kotlin/com/example/cypherspringbootcamp/controller/HelloWorldController.kt
+++ src/main/kotlin/com/example/cypherspringbootcamp/controller/HelloWorldController.kt
@@ -1,14 +1,15 @@
 package com.example.cypherspringbootcamp.controller
 
+import com.example.cypherspringbootcamp.domain.HelloWorld
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class HelloWorldController {
+private class HelloWorldController {
     @RequestMapping("/hello")
     fun greeting(
         @RequestParam(value = "name", defaultValue = "name") name: String
-    ) = "{\"message\":\"Hello, $name\"}"
+    ) = HelloWorld("Hello, $name")
 }
